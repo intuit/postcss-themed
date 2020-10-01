7e1503a5dd8058e255820f093e49e7248574889b
Reuben Evans <reuben_evans@intuit.com>
2020-10-02 00:56:33 +0100

    trying to add postcss-themed

--- examples/modern/postcss.config.js
+++ examples/modern/postcss.config.js
@@ -1,6 +1,16 @@
+const config = {
+    default: {
+      color: 'white'
+    },
+    other: {
+      color: 'black'
+    }
+  };
+
 module.exports = {
     plugins: [
         require('postcss-nested'),
         require('autoprefixer'),
+        require('postcss-themed')({ config }),
     ],
 };
