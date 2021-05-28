207732952dd58b03e6bd0c387370edb20ea8432b
Kelly Harrop <kelly_harrop@intuit.com>
2021-05-28 12:39:21 -0500

    adding tests

--- __tests__/modern.test.ts
+++ __tests__/modern.test.ts
@@ -353,7 +353,7 @@ it('Produces a single theme with dark mode if default has it', () => {
   );
 });
 
-it("Don't produce extra variables for matching values", () => {
+it("Don't produce extra variables for matching values in the default theme", () => {
   const config = {
     default: {
       light: {
@@ -1098,6 +1098,415 @@ it('can extend another theme that extends a theme', () => {
       .mytt,
       .ttlive {
         --color: teal;
+      }
+    `,
+    {
+      config,
+    }
+  );
+});
+
+import crypto from 'crypto';
+
+import { run } from './test-utils';
+
+jest.mock('browserslist', () => () => ['chrome 76']);
+
+it('Creates a simple css variable based theme', () => {
+  const config = {
+    default: {
+      color: 'purple',
+      extras: 'black',
+    },
+    mint: {
+      color: 'teal',
+    },
+  };
+
+  return run(
+    `
+      .test {
+        color: @theme color;
+        background-image: linear-gradient(to right, @theme color, @theme color)
+      }
+    `,
+    `
+      .test {
+        color: var(--color);
+        background-image: linear-gradient(to right, var(--color), var(--color))
+      }
+
+      :root {
+        --color: purple
+      }
+
+      .mint {
+        --color: teal
+      }
+    `,
+    {
+      config,
+    }
+  );
+});
+
+it('Can use alternative theme syntax', () => {
+  const config = {
+    default: {
+      color: 'purple',
+    },
+    mint: {
+      color: 'teal',
+    },
+  };
+
+  return run(
+    `
+      .test {
+        color: theme('color');
+      }
+    `,
+    `
+      .test {
+        color: var(--color, purple);
+      }
+
+      .mint {
+        --color: teal;
+      }
+    `,
+    {
+      config,
+    }
+  );
+});
+
+it('Can use alternative theme syntax - multiline', () => {
+  const config = {
+    default: {
+      color: 'purple',
+    },
+    mint: {
+      color: 'teal',
+    },
+  };
+
+  return run(
+    `
+      .test {
+        color: theme(
+          'color'
+        );
+      }
+    `,
+    `
+      .test {
+        color: var(--color, purple);
+      }
+
+      .mint {
+        --color: teal;
+      }
+    `,
+    {
+      config,
+    }
+  );
+});
+
+it('inlineRootThemeVariables false', () => {
+  const config = {
+    default: {
+      color: 'purple',
+      extras: 'black',
+    },
+    mint: {
+      color: 'teal',
+    },
+  };
+
+  return run(
+    `
+      .test {
+        color: @theme color;
+        background-image: linear-gradient(to right, @theme color, @theme color)
+      }
+    `,
+    `
+      .test {
+        color: var(--color);
+        background-image: linear-gradient(to right, var(--color), var(--color))
+      }
+
+      :root {
+        --color: purple
+      }
+
+      .mint {
+        --color: teal
+      }
+    `,
+    {
+      config,
+      inlineRootThemeVariables: false,
+    }
+  );
+});
+
+it('Creates a simple css variable based theme with light and dark', () => {
+  const config = {
+    default: {
+      light: {
+        color: 'purple',
+      },
+      dark: {
+        color: 'black',
+      },
+    },
+
+    mint: {
+      color: 'teal',
+    },
+    chair: {
+      light: {
+        color: 'beige',
+      },
+      dark: {
+        color: 'darkpurple',
+      },
+    },
+  };
+
+  return run(
+    `
+      .test {
+        color: @theme color;
+        background-image: linear-gradient(to right, @theme color, @theme color)
+      }
+    `,
+    `
+      .test {
+        color: var(--color);
+        background-image: linear-gradient(to right, var(--color), var(--color))
+      }
+      :root {
+        --color: purple
+      }
+
+      .dark {
+        --color: black
+      }
+
+      .mint.light {
+        --color: teal
+      }
+
+      .chair.light {
+        --color: beige
+      }
+
+      .chair.dark {
+        --color: darkpurple
+      }
+    `,
+    {
+      config,
+    }
+  );
+});
+
+it('Can override dark and light class', () => {
+  const config = {
+    default: {
+      light: {
+        color: 'purple',
+      },
+      dark: {
+        color: 'black',
+      },
+    },
+
+    mint: {
+      color: 'teal',
+    },
+    chair: {
+      light: {
+        color: 'beige',
+      },
+      dark: {
+        color: 'darkpurple',
+      },
+    },
+  };
+
+  return run(
+    `
+      .test {
+        color: @theme color;
+        background-image: linear-gradient(to right, @theme color, @theme color)
+      }
+    `,
+    `
+      .test {
+        color: var(--color);
+        background-image: linear-gradient(to right, var(--color), var(--color))
+      }
+      :root {
+        --color: purple
+      }
+
+      .dark-theme {
+        --color: black
+      }
+
+      .mint.light-theme {
+        --color: teal
+      }
+
+      .chair.light-theme {
+        --color: beige
+      }
+
+      .chair.dark-theme {
+        --color: darkpurple
+      }
+    `,
+    {
+      config,
+      lightClass: '.light-theme',
+      darkClass: '.dark-theme',
+    }
+  );
+});
+
+it('Produces a single theme', () => {
+  const config = {
+    default: {
+      light: {
+        color: 'purple',
+      },
+      dark: {
+        color: 'black',
+      },
+    },
+    mint: {
+      color: 'teal',
+    },
+    chair: {
+      light: {
+        color: 'beige',
+      },
+      dark: {
+        color: 'darkpurple',
+      },
+    },
+  };
+
+  return run(
+    `
+      .test {
+        color: @theme color;
+      }
+    `,
+    `
+      .test {
+        color: var(--color, beige);
+      }
+
+      .dark {
+        --color: darkpurple;
+      }
+    `,
+    {
+      config,
+      forceSingleTheme: 'chair',
+    }
+  );
+});
+
+it('Produces a single theme with dark mode if default has it', () => {
+  const config = {
+    default: {
+      light: {
+        color: 'purple',
+      },
+      dark: {
+        color: 'black',
+      },
+    },
+    mint: {
+      color: 'teal',
+    },
+  };
+
+  return run(
+    `
+      .test {
+        color: @theme color;
+      }
+    `,
+    `
+      .test {
+        color: var(--color, teal);
+      }
+
+      .dark {
+        --color: black;
+      }
+    `,
+    {
+      config,
+      forceSingleTheme: 'mint',
+    }
+  );
+});
+
+it('Theme values should override default theme even if light and dark values are the same', () => {
+  const config = {
+    default: {
+      light: {
+        color: 'purple',
+      },
+      dark: {
+        color: 'black',
+      },
+    },
+    alernate: {
+      light: {
+        color: 'blue',
+      },
+      dark: {
+        color: 'blue',
+      },
+    },
+  };
+
+  return run(
+    `
+      .test {
+        color: @theme color;
+        background-image: linear-gradient(to right, @theme color, @theme color)
+      }
+    `,
+    `
+      .test {
+        color: var(--color);
+        background-image: linear-gradient(to right, var(--color), var(--color))
+      }
+      :root {
+        --color: purple
+      }
+
+      .dark {
+        --color: black
+      }
+
+      .alternate.light {
+        --color: blue
+      }
+
+      .alternate.dark {
+        --color: blue
       }
     `,
     {
